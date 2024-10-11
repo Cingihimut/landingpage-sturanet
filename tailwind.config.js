e0460220ec965f99820cd2f002f7ca3534585335
cingihimut <cingihimutbanget@gmail.com>
2024-10-12 01:06:36 +0700

    update nextra style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ const flowbite = require("flowbite-react/tailwind");
 
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,md,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,md,mdx}",
     flowbite.content(),
   ],
   theme: {
